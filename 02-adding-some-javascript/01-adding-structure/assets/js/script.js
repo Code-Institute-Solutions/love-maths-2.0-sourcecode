c4685ec856f7029c8b58a684939c8513ac305ba6
Anna Greaves <gilhespy.anna@gmail.com>
2021-01-28 13:49:52 +0100

    Add sourcecode for adding javascript structure lesson.

---
+++ 02-adding-some-javascript/01-adding-structure/assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
