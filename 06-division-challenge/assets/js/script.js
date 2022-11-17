8cfc1f1f64b1bc706bdd94c5934407f23bf016e0
Joke Heyndels <jo_hannah@outlook.com>
2022-11-17 10:07:45 +0000

    Replace tabs with spaces to fix indentation on Github

--- 06-division-challenge/assets/js/script.js
+++ 06-division-challenge/assets/js/script.js
@@ -146,9 +146,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 // Add your division question here
 function displayDivisionQuestion(operand1, operand2) {
-    operand1 = operand1*operand2;
-	document.getElementById("operand1").textContent = operand1;
-	document.getElementById("operand2").textContent = operand2;
-	document.getElementById("operator").textContent = "/";
-
+    operand1 = operand1 * operand2;
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
