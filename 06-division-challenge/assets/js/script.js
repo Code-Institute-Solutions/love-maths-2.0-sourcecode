f7c18c924a4e4141848af8882026e966cea159d5
Joke Heyndels <jo_hannah@outlook.com>
2022-11-17 10:03:55 +0000

    Add missing semi-colon in solution code to help with correct indentation on Github

--- 06-division-challenge/assets/js/script.js
+++ 06-division-challenge/assets/js/script.js
@@ -146,7 +146,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 // Add your division question here
 function displayDivisionQuestion(operand1, operand2) {
-    operand1 = operand1*operand2
+    operand1 = operand1*operand2;
 	document.getElementById("operand1").textContent = operand1;
 	document.getElementById("operand2").textContent = operand2;
 	document.getElementById("operator").textContent = "/";
